2150dd3022abb43e090f786db999fbc586adc18c
Alexey <uniwallkman@gmail.com>
2025-08-01 11:13:43 +0400

    Homework 11 init

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,6 +34,7 @@ include("L20-hibernate")
 include("L21-jpql:class-demo")
 include("L21-jpql:homework")
 include("L22-cache")
+include("hw11-cache")
 
 pluginManagement {
     val jgitver: String by settings
